a9efb4e02e43b7091b28e02ad45f2d96be077ae5
Amad Siddiqui <80278397+Amad-Ahmed@users.noreply.github.com>
2023-07-27 14:15:22 +0500

    vite, React along with Tailwind css

---
+++ amad-portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline italic">Hello world!</h1>
+      <p className="font-bold text-2xl text-red-900 border">Fuck</p>
+    </>
+  );
+}
+
+export default App;
