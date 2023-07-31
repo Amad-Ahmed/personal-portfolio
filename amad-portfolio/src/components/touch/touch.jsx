18927be3ddac97d43e4890f74683525c21f0631b
Amad Siddiqui <80278397+Amad-Ahmed@users.noreply.github.com>
2023-07-31 22:04:10 +0500

    Responsiveness

--- amad-portfolio/src/components/touch/touch.jsx
+++ amad-portfolio/src/components/touch/touch.jsx
@@ -8,8 +8,7 @@ function Touch() {
           Get in touch
         </h2>
         <p class="mb-8 lg:mb-16 font-light text-center text-black dark:text-black sm:text-xl">
-          Got a technical issue? Want to send feedback about a beta feature?
-          Need details about our Business plan? Let us know.
+          Got a Project? Need help with something? Feel free to reach out to me
         </p>
         <form action="#" class="space-y-8">
           <div>
@@ -23,7 +22,7 @@ function Touch() {
               type="email"
               id="email"
               class="shadow-sm bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="name@flowbite.com"
+              placeholder="name@xyz.com"
               required
             ></input>
           </div>
@@ -38,7 +37,7 @@ function Touch() {
               type="text"
               id="subject"
               class="block p-3 w-full text-sm text-black bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-              placeholder="Let us know how we can help you"
+              placeholder="Let me know how I can help you"
               required
             ></input>
           </div>
